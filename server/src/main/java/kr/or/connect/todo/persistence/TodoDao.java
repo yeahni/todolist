f9b9c73c0ddc0fd51e83e9ece04ce5b8b4ef1bd3
Yeeun <Yeeun@218.38.137.28>
2017-06-06 05:28:49 +0900

    3. TodoSqls 및 TodoDao 클래스 제작하기

--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -1,20 +1,63 @@
 package kr.or.connect.todo.persistence;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import kr.or.connect.todo.domain.Todo;
 
 @Repository
 public class TodoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
+	private RowMapper<Todo> rowMapper = BeanPropertyRowMapper.newInstance(Todo.class);
 	
 	public TodoDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 		this.insertAction = new SimpleJdbcInsert(dataSource)
 				.withTableName("todo")
 				.usingGeneratedKeyColumns("id");
+	}
+	
+	public int countTodos() {
+		Map<String, Object> params = Collections.emptyMap();
+		return jdbc.queryForObject(TodoSqls.COUNT_TODO, params, Integer.class);
+	}
+	
+	public Todo selectById(Integer id){
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", id);
+		return jdbc.queryForObject(TodoSqls.SELECT_BY_ID, params, rowMapper);
+	}
+	
+	public List<Todo> selectAll(){
+		Map<String, Object> params = Collections.emptyMap();
+		return jdbc.query(TodoSqls.SELECT_ALL, params, rowMapper);
+	}
+	
+	public Integer insert(Todo todo){
+		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
+		return insertAction.executeAndReturnKey(params).intValue();
+	}
+	
+	public int update(Todo todo) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
+		return jdbc.update(TodoSqls.UPDATE, params);
+	}
+	
+	public int deleteById(Integer id) {
+		Map<String, Object> params = Collections.singletonMap("id", id);
+		return jdbc.update(TodoSqls.DELETE_BY_ID, params);
 	}
 }
--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,6 +1,16 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
+	static final String COUNT_TODO = 
+			"SELECT COUNT(*) FROM todo";
+	static final String SELECT_ALL = 
+			"SELECT * FROM todo ORDER BY date DESC";
+	static final String SELECT_BY_ID =
+			"SELECT * FROM todo WHERE id = :id";
+	static final String UPDATE=
+			"UPDATE todo SET\n"
+			+ "completed = :completed\n"
+			+ "WHERE id = :id";
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
 }
