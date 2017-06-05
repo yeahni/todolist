c4b6bebd672c211c79be089a76afd3bf2493d5eb
Yeeun <Yeeun@218.38.137.28>
2017-06-06 05:35:21 +0900

    5. TodoController 클래스 제작하기

---
+++ server/src/main/java/kr/or/connect/todo/api/TodoController.java
@@ -0,0 +1,58 @@
+package kr.or.connect.todo.api;
+
+import java.util.Collection;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.connect.todo.domain.Todo;
+import kr.or.connect.todo.service.TodoService;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+	private final TodoService service;
+	
+	@Autowired
+	public TodoController(TodoService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/{id}")
+	Todo read(@PathVariable Integer id){
+		return service.findById(id);
+	}
+	
+	@GetMapping
+	Collection<Todo> readList(){
+		return service.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	Todo create(@RequestBody Todo todo){
+		return service.create(todo);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void update(@PathVariable Integer id, @RequestBody Todo todo) {
+		todo.setId(id);
+		service.update(todo);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void delete(@PathVariable Integer id) {
+		service.delete(id);
+	}
+}
